aacf03e7c872a020a429f2fcd4eab1371537cfac
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-30 08:07:10 +0530

    SetState

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Greet } from './components/Greet';
 import Hello from './components/Hello';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet name="Bruce" heroname="Doremon"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam commodi excepturi tenetur ipsam voluptas amet.</p> </Greet>
       <Greet name="clark" heroname="Superman"><button>Button
       </button></Greet>
