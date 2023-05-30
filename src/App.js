6fbbe46f73b3cf5ef053c08c3b083c43736c5ef7
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-30 18:33:17 +0530

    Refs

--- src/App.js
+++ src/App.js
@@ -20,12 +20,14 @@ import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 
 function App() {
   return (
     <div className="App">
-      <ParentComp />
+      <RefsDemo />
+      {/* <ParentComp /> */}
       {/* <PureComp /> */}
       {/* <Table /> */}
       {/* <FragmentDemo /> */}
