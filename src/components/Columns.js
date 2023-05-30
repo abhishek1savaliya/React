b1b3c07b3160055528960b8bd424d669070bc87d
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-30 17:43:02 +0530

    Fragments

---
+++ src/components/Columns.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Columns() {
+    const items = []
+  return (
+    <>
+      <td>Name</td>
+      <td>Krishna</td>
+    </>
+    
+  )
+}
+
+export default Columns
