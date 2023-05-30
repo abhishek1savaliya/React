9e5ecf40da97ba7597073db798c193114320b2c0
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-30 14:13:29 +0530

    Index as Key Anti-pattern

--- src/components/List.js
+++ src/components/List.js
@@ -2,28 +2,29 @@ import React from 'react'
 import Person from './Person'
 
 function List() {
+    const names = ['Krishna', 'Arjuna', 'Hanuman'];
     const persons = [
         {
-            id:1,
-            name:"Krishna",
-            age : 45,
-            skill : "All Technology"
+            id: 1,
+            name: "Krishna",
+            age: 45,
+            skill: "All Technology"
         },
         {
-            id:2,
-            name:"Arjun",
-            age : 39,
-            skill : "Arrow"
+            id: 2,
+            name: "Arjun",
+            age: 39,
+            skill: "Arrow"
         },
         {
-            id:3,
-            name:"Hanuman",
-            age : 5000,
-            skill : "Gada"
+            id: 3,
+            name: "Hanuman",
+            age: 5000,
+            skill: "Gada"
         }
     ]
-    const  PersonList = persons.map(persons => <Person key={persons.id} persons={persons}/>)
-    return (<div>{PersonList}</div>)
+    const NamesList = names.map((name,index) => <h2 key={index}>{index} {name}</h2> )
+    return (<div>{NamesList}</div>)
 }
 
 export default List
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Person = (props) => {
-    const { name, age, skill } = props.persons 
+    const { name, age, skill } = props.persons
 
-  return (
-    <div>
-      <div>I am {name}. I am {age} years Old ans I know {skill}</div>
-    </div>
-  )
+    return (
+        <div>
+            <div>I am {name}. I am {age} years Old ans I know {skill}</div>
+        </div>
+    )
 }
 
 export default Person
