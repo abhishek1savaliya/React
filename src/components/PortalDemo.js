e943f0b57d2c4b8e6f23be0c49340c3dfc07c33c
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-05-31 09:09:49 +0530

    Portal

---
+++ src/components/PortalDemo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createPortal } from 'react-dom';
+
+function PortalDemo() {
+  return createPortal (
+  <h1>Portals demo</h1>,document.getElementById('portal-root')
+  )
+}
+
+export default PortalDemo
